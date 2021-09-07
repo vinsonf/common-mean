cc7ac914bcb1a119db21179be58b9b0b91be8899
ChelleAmon <amongichelle@gmail.com>
2021-09-07 13:11:48 -0700

    :zap: server.ts - declared variable 'port'

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-// const port = 3502;
+const port = 3502;
 
 app.get('/', function (req,res){
     console.log('app');
     res.json({message:'test'})
 })
 
-app.listen(3502, function(){
- console.log(`running on port http://localhost:3502`)
+app.listen(port, function(){
+ console.log(`running on port http://localhost:${port}`)
 })
