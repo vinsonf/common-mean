e6d193aa8f69f45a09909b4a33fa4b3ceecdfef5
ChelleAmon <amongichelle@gmail.com>
2021-09-07 13:26:41 -0700

    :heavy_plus_sign: server.ts - added app.get method

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import cors from "cors";
+import path, {resolve} from "path";
 
 const app = express();
 const port = 3502;
+const __dirname = resolve();
+
+
+app.use(cors());
 
 app.get('/', function (req,res){
     console.log('app');
     res.json({message:'test'})
+})
+
+
+app.get('/users', function(req,res){
+    res.sendFile(path.join(__dirname, 'users.json'))
 })
 
 app.listen(port, function(){
