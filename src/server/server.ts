1493649c6f56ab760f6d5181a86f92435e828547
sabiyoung <ethiodatecakes@gmail.com>
2021-09-07 13:17:50 -0700

    create get users method

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3002;
-
+const __dirname = path.resolve()
 app.listen(port, function() {
     console.log(`runnimg on http://localhost: ${port}`)
 })
+
+
+app.get('/users', function(req, res) {
+    res.sendFile(path.join(__dirname, 'users.json'))
+})
+
 app.get('/', function(req, res) {
     console.log('my app')
    res.json({user: 'test'})
