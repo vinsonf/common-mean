df2c4c56e6a6290324d650be62e9118e5884906a
LeeTato <tab271621@gmail.com>
2021-09-07 16:02:19 -0500

    create listner

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,12 @@
 import express from "express";
+import path from "path";
+const __dirname = path.resolve();
+const port =3000;
+
+
 
 const app = express();
+ 
+app.listen(port, function() {
+    console.log(`port runinig on http://localhost: ${port}`)
+})
