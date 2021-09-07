33dd34e5aafc7f93e1162850b925aef841231dae
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-09-07 14:10:13 -0700

    add users.json and users get route

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,6 +11,9 @@ app.use(cors)
 app.get('/', function(req, res) {
     res.json({message: 'up and running'})
 })
+app.get('/users', function(req, res) {
+    res.sendFile(path.join(__dirname, 'users.json'));
+})
 
 app.listen(port, function() {
     console.log('running on http://localhost:'+ port) 
