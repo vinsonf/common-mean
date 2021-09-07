bba6679f6ce23c9e948defe69ff2df67b9bed791
ChelleAmon <amongichelle@gmail.com>
2021-09-07 12:26:32 -0700

    :heavy_plus_sign: main.ts - imported express server; server.ts- added get and listen call

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,2 +1,14 @@
 import express from "express";
 import cors from "cors";
+
+const app = express();
+// const port = 3502;
+
+app.get('/', function (req,res){
+    console.log('app');
+    res.json({message:'test'})
+})
+
+app.listen(3502, function(){
+ console.log(`running on port http://localhost:3502`)
+})
