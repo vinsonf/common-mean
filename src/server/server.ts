4057240574c27e41e167971b2ac533ff718c720f
vinsonf <vfernandez@shift3tech.com>
2021-09-07 11:13:09 -0700

    set app listen

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,10 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.json({message: 'working'})
+});
+
+app.listen(3002, function() {
+    console.log('running on port', 3002);
 })
 
 
