9d2016bcc688b64c8fa57ac9924938c79fda336f
LeeTato <tab271621@gmail.com>
2021-09-07 16:03:14 -0500

    create app.get

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,6 +6,12 @@ const port =3000;
 
 
 const app = express();
+
+app.get('/', function(req, res) {
+    console.log('my app')
+   res.json({user: 'test'})
+  });
+
  
 app.listen(port, function() {
     console.log(`port runinig on http://localhost: ${port}`)
