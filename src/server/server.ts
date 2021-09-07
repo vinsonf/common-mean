93c4dec4444b28c5aea130545a0c9f3197ec22bb
sabiyoung <ethiodatecakes@gmail.com>
2021-09-07 13:10:13 -0700

    create get method

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
 const app = express();
-
 const port = 3002;
 
 app.listen(port, function() {
     console.log(`runnimg on http://localhost: ${port}`)
 })
+app.get('/', function(req, res) {
+    console.log('my app')
+   res.json({user: 'test'})
+  });
