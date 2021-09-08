9b365667cbfa9d4fd40c9da135c01da36b33422e
sirisha v <svoggu@alphaworks.tech@gmail.com>
2021-09-07 20:26:14 -0700

    listening to port 3000

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,5 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.json({message: 'working'})
+    res.json({message: 'working'});
 })
+
+app.listen(3000, function() {
+    console.log('running on port', 3000);
+}
