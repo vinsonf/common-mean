87a65e128bd06a613440775bd97437bf736925f6
sabiyoung <ethiodatecakes@gmail.com>
2021-09-07 17:58:50 -0700

    impoted HttpClientModule & Form module

--- src/client/src/app/services/todo.service.ts
+++ src/client/src/app/services/todo.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+private _todo: Todo [] = []
+  constructor(private api: ApiService) {
+this.getTodo()
+   }
 
-  constructor() { }
+   get todos(){
+     return this._todo
+   }
+   getTodo(){
+ this.api.get<Todo[]>('todos').subscribe(todos => {
+   this._todo = todos
+ })
+   }
 }
