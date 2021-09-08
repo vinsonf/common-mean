a5a388e1d17828a719f1cc5a97e72ba8b7672cb4
mekdes45 <mekmis2020@gmail.com>
2021-09-07 17:13:01 -0700

    export todo class

---
+++ src/client/src/app/model/todo.ts
@@ -0,0 +1,8 @@
+export class Todo {
+    constructor(public userId: number,
+        public id: number,
+        public title: string,
+        public completed: boolean = false) {
+        
+    }
+}
