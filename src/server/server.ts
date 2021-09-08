29ded1a7db2cde0e96307199d69c51f630cc9725
LeeTato <tab271621@gmail.com>
2021-09-08 14:27:26 -0500

    create post component ,postservice,apiservice,models, and post backend

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,22 @@
 import express from "express";
 import path from "path";
+import cors from "cors"
 const __dirname = path.resolve();
 const port =3001;
 
-const app = express();
 
+const app = express();
+app.use(cors());
 app.get('/', function(req, res) {
    res.json({test: 'test'})
   });
 
   app.get('/users',function(req,res){
       res.sendFile(path.join(__dirname,'users.json'))
+  });
+
+  app.get('/posts',function(req,res){
+      res.sendFile(path.join(__dirname,'posts.json'));
   });
  
 app.listen(port, function() {
