e3c9072409a7dd66ca20128d94d66b8f576c18a9
sabiyoung <ethiodatecakes@gmail.com>
2021-09-07 17:42:17 -0700

    created api service file, baseUrl and get method

---
+++ src/client/src/app/services/api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl: string = 'http://localhost:3002/'
+  constructor(private http: HttpClient) {
+
+   }
+
+get<T>(resource: string) {
+  return this.http.get<T>((this.baseUrl + resource))
+}
+}
