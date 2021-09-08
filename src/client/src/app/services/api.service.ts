24ba0cbabef206d0abf9c1a189580fba00b132d2
ChelleAmon <amongichelle@gmail.com>
2021-09-07 20:59:57 -0700

    refactor(api): created fn to get data from db

--- src/client/src/app/services/api.service.ts
+++ src/client/src/app/services/api.service.ts
@@ -6,8 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
+  private baseUrl= 'http://localhost:3502/';
   constructor(private http: HttpClient) { }
 
-
+  getUser<T>(resource:string){
+    this.http.get<T>(this.baseUrl + resource)
+  }
 
 }
