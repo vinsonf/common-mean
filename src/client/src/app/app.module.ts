2a825f71f391c327fffa1f76304047652d021462
mekdes45 <mekmis2020@gmail.com>
2021-09-07 17:42:18 -0700

    import HttpclientModule inside app.module

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './componenets/todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { TodoListComponent } from './componenets/todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/client/src/app/services/todo.service.ts
+++ src/client/src/app/services/todo.service.ts
@@ -1,9 +1,10 @@
+import { Todo } from './../model/todo';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-
+  private _todo: Todo[] = [];
   constructor() { }
 }
