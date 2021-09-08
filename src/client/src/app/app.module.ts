c3ee1f2ecb51602df4981d4cbe777d2cc2a46dd4
ChelleAmon <amongichelle@gmail.com>
2021-09-07 20:31:53 -0700

    refactor(user-list): imported user components into @NgModule

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
