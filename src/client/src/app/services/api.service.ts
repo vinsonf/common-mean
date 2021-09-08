a6dfedf40a655c0fd1612b9a372b4834ac02aa59
ChelleAmon <amongichelle@gmail.com>
2021-09-07 21:01:49 -0700

    refactor(api): added a space and 'return' keyword

--- src/client/src/app/services/api.service.ts
+++ src/client/src/app/services/api.service.ts
@@ -7,10 +7,11 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   private baseUrl= 'http://localhost:3502/';
+
   constructor(private http: HttpClient) { }
 
   getUser<T>(resource:string){
-    this.http.get<T>(this.baseUrl + resource)
+   return this.http.get<T>(this.baseUrl + resource)
   }
 
 }
