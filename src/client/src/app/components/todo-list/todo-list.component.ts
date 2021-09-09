29cbb4c5e311eaef4e18bb36adae768c3904f1ee
ChelleAmon <amongichelle@gmail.com>
2021-09-09 12:33:26 -0700

    refactor(todo): asserted todos$.assigned  observable

--- src/client/src/app/components/todo-list/todo-list.component.ts
+++ src/client/src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from 'src/app/services/todo.service';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,6 +9,8 @@ import { TodoService } from 'src/app/services/todo.service';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public todos$!: Observable<Todo[]>
 
   constructor(private todoService: TodoService) { }
 
