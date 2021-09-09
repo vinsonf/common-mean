2e4dd50c23ed4b51af914bbd11c3b94215bf9a9c
ChelleAmon <amongichelle@gmail.com>
2021-09-09 12:15:45 -0700

    feat(todo): injected apiservice into constructor

--- src/client/src/app/services/todo.service.ts
+++ src/client/src/app/services/todo.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  private _todos: Todo[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+
 }
