8972cf2af640da955ef94e0d31e6e82625ccc9cd
ChelleAmon <amongichelle@gmail.com>
2021-09-09 12:28:16 -0700

    feat(todo): injected todoservice into constructor

--- src/client/src/app/components/todo-list/todo-list.component.ts
+++ src/client/src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
   }
