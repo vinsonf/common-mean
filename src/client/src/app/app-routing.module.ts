f4bfc9ed511f6964b43c00cfb6c286ff6ed64275
ChelleAmon <amongichelle@gmail.com>
2021-09-09 14:14:53 -0700

    feat(routing): added routes for users and todos

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/users', component: UserListComponent},
+  {path: '/todos', component: TodoListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
